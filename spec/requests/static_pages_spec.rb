1769ce81b8ba759a3906e3dd6953e7dc91465b2d
malcolm <mparrish91@gmail.com>
2013-12-14 18:58:28 -0600

    Adding HTML/finishing static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Today'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Today')
+  end
+
+      it "should have a title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end
+   end
+
+    describe "About" do
+
+    it "should have the content 'A Powerful Message'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('A Powerful Message')
+	end
+
+      it "should have a title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
+    end
+end
+end
